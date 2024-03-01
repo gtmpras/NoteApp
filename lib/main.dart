08bfdbe6e499803d8237a154d8ebd8860e57e116
Prasoon Gautam <109397711+gtmpras@users.noreply.github.com>
2024-03-01 22:53:58 +0545

    keeping user logged in

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:note_app/firebase_options.dart';
+import 'package:note_app/views/homeScreen.dart';
 import 'package:note_app/views/signIn_screen.dart';
 
 void main() async{
@@ -10,15 +12,30 @@ void main() async{
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+
+  User? user;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    user=FirebaseAuth.instance.currentUser;
+    print(user?.uid.toString());
+  }
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: SignInScreen(),
+      home: user != null ? const HomeScreen() : const SignInScreen(),
     );
   }
 }
