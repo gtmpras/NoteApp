9628c8f1cf960f1d53cb74da99e2d9a48218030d
Prasoon Gautam <109397711+gtmpras@users.noreply.github.com>
2024-03-03 22:21:00 +0545

    data fetched from firebase

--- lib/views/homeScreen.dart
+++ lib/views/homeScreen.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:note_app/views/createNoteScreen.dart';
@@ -12,6 +14,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  User? userId = FirebaseAuth.instance.currentUser;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,10 +29,46 @@ class _HomeScreenState extends State<HomeScreen> {
               child: Icon(Icons.logout)),
         ],
       ),
-      body: Center(
-        child: Container(
-          child: Text("Notes"),
-        ),
+      body: Container(
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+          .collection("notes")
+          .where("userid", isEqualTo:userId?.uid)
+          .snapshots(),
+          builder: ((context,AsyncSnapshot<QuerySnapshot> snapshot) {
+            
+            if(snapshot.hasError){
+              return Text('Something went wrong!');
+            }
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return Center(child: CupertinoActivityIndicator());
+            }
+            if(snapshot.data!.docs.isEmpty){
+              return Center(child: Text("No data found"));
+            }
+            if(snapshot !=null && snapshot.data != null){
+              return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context,index){
+                  var note = snapshot.data!.docs[index]['note'];
+                  var userid = snapshot.data!.docs[index]['userid'];
+                  return Card(child: ListTile(
+                    title: Text(note),
+                    subtitle: Text(userid),
+                    trailing: Row(
+                      mainAxisSize:MainAxisSize.min ,
+                      children: [
+                        Icon(Icons.edit),
+                        SizedBox(width:10),
+                        Icon(Icons.delete)
+                      ],
+                    ),
+                  ),);
+              });
+              
+            }
+            return Container();
+          })),
       ),
       floatingActionButton: FloatingActionButton(onPressed: (){
         Get.to(()=> CreateNoteScreen());
